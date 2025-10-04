d8f359507a994f163aeae0a45472c6058f310a55
= <adammalota@mansio.pl>
2025-10-05 01:28:38 +0200

    path

--- src/main/frontend/src/app/component/tile/tile.component.ts
+++ src/main/frontend/src/app/component/tile/tile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, input, Output, ViewChild } from '@angular/core';
 import { applyPixelArtBackground } from 'src/app/utils/pixelartHelper';
 
 @Component({
@@ -16,8 +16,26 @@ export class TileComponent {
 
   height = input()
   color = input()
-  class = input()
+ // class = input()
   display = input()
+
+  @Input() x: number;
+  @Input() y: number;
+  @Input() class!: string;
+
+  @Input() selected = false;
+@Input() endSelected = false;
+@Input() inPath: boolean = false;
+
+
+
+
+  @Output() tileClicked = new EventEmitter<{ x: number, y: number }>();
+
+  onClick(): void {
+    this.tileClicked.emit({ x: this.x, y: this.y});
+    console.log("clicked",this.x,this.y)
+  }
 
   imgVisibleHouse(){
     return Math.random() < 0.1
